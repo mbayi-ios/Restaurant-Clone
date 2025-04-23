7e10e059cf28af29177e99f4a8c741524f3dc3c3
Ambrose <ambymbayi45@gmail.com>
2025-04-23 09:18:36 +0300

    Update HTTPRequest.swift

--- lunchbox/Network/HTTP/HTTPRequest/HTTPRequest.swift
+++ lunchbox/Network/HTTP/HTTPRequest/HTTPRequest.swift
@@ -1,15 +1,10 @@
-//
-//  HTTPRequest.swift
-//  lunchbox
-//
-//  Created by Ambrose Mbayi on 21/04/2025.
-//
-
+import Foundation
 
 protocol HTTPRequest {
+
     associatedtype Payload: Encodable
     associatedtype Response: Decodable
-    
+
     var method: HTTPMethod { get }
     var path: HTTPEndpoint { get }
     var body: Payload? { get }
